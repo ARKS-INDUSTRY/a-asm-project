b19c42f22c5f1308dc42014a5bf3db0ab953df8f
RAKESH Dev <59530718+ARKS-INDUSTRY@users.noreply.github.com>
2022-11-02 17:49:33 +0530

    Ok

--- ams/hello.py
+++ ams/hello.py
@@ -2,7 +2,7 @@ import os
 path = os.getcwd()
 os.system(f'cd {path}')
 os.system("sudo apt install nsam")
-os.system("nasm -f elf32 -o hello_world_test.o hello.asm")
+#os.system("nasm -f elf32 -o hello_world_test.o hello.asm")
 os.system("ld -m elf_i386 -o hello_test hello_world_test.o ")
 os.system("./hello_test")
 os.system("echo LOL")
